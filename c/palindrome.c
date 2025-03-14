bdda94defd2259fccffd047f59d12b569cdf2e0f
Eduardo Gerhardt <comercial.eduardogerhardt@gmail.com>
2025-03-14 18:23:57 -0300

    fix: fix palindrome checker

--- c/palindrome.c
+++ c/palindrome.c
@@ -2,21 +2,23 @@
 #include <string.h>
 #include <stdbool.h>
 
-bool CheckIsPalindrome() {
-  char str[] = "arara";
-
-  for (int i=0; i < 5; i++) {
-    int size = strlen(str);
+bool CheckIsPalindrome(char str[]) {
+  int size = strlen(str);
+  for (int i=0; i < size; i++) {
     if (!(str[i] == str[((size - 1) - i)])) {
       return false;
     }
   }
-
   return true;
 }
 
 int main() {
-  if (CheckIsPalindrome()) {
+  char word[15];
+
+  printf("Input:");
+  scanf("%s", word);
+
+  if (CheckIsPalindrome(word)) {
     printf("# Is Palindrome! :)\n");
   } else {
     printf("# Isn't Palindrome! :(\n");
