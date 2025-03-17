e759c7d1026c60992ebd9f67ba5918621a1e1378
Eduardo Gerhardt <comercial.eduardogerhardt@gmail.com>
2025-03-16 21:08:52 -0300

    feat: add errors count

--- cpp/hangman.cpp
+++ cpp/hangman.cpp
@@ -3,35 +3,47 @@
 
 using namespace std;
 
-#define word "engineering"
+#define SECRET_WORD "engineering"
+#define MAX_ERRORS 7
 
 void header(int size) {
   cout << "#***************************************#" << endl;
   cout << "#                HANGMAN                #" << endl;
   cout << "#***************************************#" << endl;
-  cout << "# " << size << " letras" << endl;
+  cout << "# " << size << " letras / " << " Máximos de erros: " << MAX_ERRORS  << endl;
 
   for (int i=0; i < size; i++) {
     cout << "_ ";
   }
+  cout << endl << endl;
 }
 
 int main() {
   char letter;
-  int size = strlen(word);
+  int size = strlen(SECRET_WORD);
+  int errors = 0;
 
   header(size);
  
-
-  while (true) { 
-    cout << "\n\nChute uma letra: ";
+  while (errors < MAX_ERRORS) {
+    int count = 0;
+    cout << "Erros: " << errors << endl;
+    cout << "Chute uma letra: ";
     cin >> letter;
 
     for (int i=0; i < size; i++) {
-      if (letter == word[i]) {
-        cout << letter << endl;
+      if (letter == SECRET_WORD[i]) {
+        cout << letter << " ";
+        count++;
+      } else {
+        cout << "_ ";
       }
     }
+
+    if (count == 0) {
+      errors++;
+    }
+    cout << endl << endl;
   }
 
 
